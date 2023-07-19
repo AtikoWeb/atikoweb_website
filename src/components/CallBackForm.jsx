12b8cd44f0b14b17a7f32e199ec8be0eb7aef264
AtikoWeb <dev.atikoweb@gmail.com>
2023-07-19 21:12:30 +0600

    Update CallBackForm.jsx

--- src/components/CallBackForm.jsx
+++ src/components/CallBackForm.jsx
@@ -10,6 +10,7 @@ sgMail.setApiKey(
 export default function CallBackForm({ open = false, onClose }) {
 	const [name, setName] = useState('');
 	const [phone, setPhone] = useState('');
+	const [email, setEmail] = useState('');
 	const cancelButtonRef = useRef(null);
 
 	const handleSubmit = async (e) => {
@@ -72,7 +73,7 @@ export default function CallBackForm({ open = false, onClose }) {
 								>
 									<IoCloseCircleSharp
 										size={40}
-										className='text-gray-500'
+										className='text-black/[.5]'
 									/>
 								</div>
 								<div className='mb-5'>
@@ -85,7 +86,7 @@ export default function CallBackForm({ open = false, onClose }) {
 									<input
 										type='name'
 										id='name'
-										className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+										className='shadow-sm bg-black/[.1] border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
 										placeholder='Иван Иванов'
 										value={name}
 										onChange={(e) => setName(e.target.value)}
@@ -101,10 +102,26 @@ export default function CallBackForm({ open = false, onClose }) {
 									<input
 										type='phone'
 										id='phone'
-										className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+										className='shadow-sm bg-black/[.1] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
 										placeholder='+7 777 77-77'
 										value={phone}
 										onChange={(e) => setPhone(e.target.value)}
+									/>
+								</div>
+								<div className='mt-5'>
+									<label
+										for='phone'
+										className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+									>
+										Email
+									</label>
+									<input
+										type='email'
+										id='email'
+										className='shadow-sm bg-black/[.1] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+										placeholder='example@mail.ru'
+										value={email}
+										onChange={(e) => setEmail(e.target.value)}
 									/>
 								</div>
 								<div className='flex justify-center mt-8'>
