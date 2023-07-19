d74621b63b72fabb397bfa7f362798bc5d43be04
AtikoWeb <dev.atikoweb@gmail.com>
2023-07-19 20:38:48 +0600

    Update CallBackForm.jsx

--- src/components/CallBackForm.jsx
+++ src/components/CallBackForm.jsx
@@ -2,7 +2,6 @@ import { useRef, useState, Fragment } from 'react';
 import sgMail from '@sendgrid/mail';
 import { Transition, Dialog } from '@headlessui/react';
 import { IoCloseCircleSharp } from 'react-icons/io5';
-import axios from 'axios';
 
 sgMail.setApiKey(
 	'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'
@@ -17,18 +16,14 @@ export default function CallBackForm({ open = false, onClose }) {
 		e.preventDefault();
 
 		try {
-			const url = 'http://api.sendgrid.com/v3/mail/send';
-
-			const data = {
-				personalizations: [{ to: [{ email: 'timmy.shiyanov@gmail.com' }] }],
-				from: { email: 'dev.atikoweb@gmail.com' },
+			const msg = {
+				to: 'timmy.shiyanov@gmail.com', // Ваш email
+				from: 'dev.atikoweb@gmail.com', // Ваш SendGrid Sender
 				subject: 'Новая заявка',
-				content: [
-					{ type: 'text/plain', value: `Имя: ${name}\nТелефон: ${phone}` },
-				],
+				text: `Имя: ${name}\nТелефон: ${phone}`,
 			};
 
-			await axios.post(url, data);
+			await sgMail.send(msg);
 			setName('');
 			setPhone('');
 			onClose();
@@ -36,7 +31,6 @@ export default function CallBackForm({ open = false, onClose }) {
 			console.error('Ошибка при отправке данных', error);
 		}
 	};
-
 	return (
 		<Transition.Root
 			show={open}
@@ -71,7 +65,7 @@ export default function CallBackForm({ open = false, onClose }) {
 							leaveFrom='opacity-100 translate-y-0 sm:scale-100'
 							leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
 						>
-							<Dialog.Panel className='relative transform w-screen p-12 overflow-hidden rounded-3xl backdrop-blur-2xl bg-white/40 dark:bg-black/70 text-left shadow-2xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
+							<Dialog.Panel className='relative transform w-screen p-12 overflow-hidden rounded-3xl backdrop-blur-2xl bg-white/[.6] dark:bg-black/70 text-left shadow-2xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
 								<div
 									className='absolute right-2 top-2 btn btn-ghost btn-circle'
 									onClick={onClose}
@@ -82,26 +76,32 @@ export default function CallBackForm({ open = false, onClose }) {
 									/>
 								</div>
 								<div className='mb-5'>
-									<label className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>
+									<label
+										for='name'
+										className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+									>
 										Имя
 									</label>
 									<input
 										type='name'
 										id='name'
-										className='shadow-sm bg-black/10 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-white/10   dark:placeholder-gray-400ß'
+										className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
 										placeholder='Иван Иванов'
 										value={name}
 										onChange={(e) => setName(e.target.value)}
 									/>
 								</div>
 								<div>
-									<label className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>
+									<label
+										for='phone'
+										className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+									>
 										Телефон
 									</label>
 									<input
 										type='phone'
 										id='phone'
-										className='shadow-sm bg-black/10  text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-white/10  dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+										className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
 										placeholder='+7 777 77-77'
 										value={phone}
 										onChange={(e) => setPhone(e.target.value)}
@@ -111,7 +111,6 @@ export default function CallBackForm({ open = false, onClose }) {
 									<button
 										type='submit'
 										className='btn btn-primary text-[15px] normal-case btn-wide font-bold'
-										onClick={handleSubmit}
 									>
 										Хочу попробовать
 									</button>
