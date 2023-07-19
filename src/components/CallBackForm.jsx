199fc11265460e4b57b96f0f7ccff1c35a027cce
AtikoWeb <dev.atikoweb@gmail.com>
2023-07-19 21:34:48 +0600

    Update CallBackForm.jsx

--- src/components/CallBackForm.jsx
+++ src/components/CallBackForm.jsx
@@ -66,14 +66,14 @@ export default function CallBackForm({ open = false, onClose }) {
 							leaveFrom='opacity-100 translate-y-0 sm:scale-100'
 							leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
 						>
-							<Dialog.Panel className='relative transform w-screen p-12 overflow-hidden rounded-3xl backdrop-blur-2xl bg-white/[.6] dark:bg-black/70 text-left shadow-2xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
+							<Dialog.Panel className='relative transform w-screen p-12 overflow-hidden rounded-3xl backdrop-blur-2xl bg-white/[.6] dark:bg-black/[.6] text-left shadow-2xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
 								<div
 									className='absolute right-2 top-2 btn btn-ghost btn-circle'
 									onClick={onClose}
 								>
 									<IoCloseCircleSharp
 										size={40}
-										className='text-black/[.5]'
+										className='text-black/[.5] dark:text-white/[.5]'
 									/>
 								</div>
 								<div className='mb-5'>
@@ -86,7 +86,7 @@ export default function CallBackForm({ open = false, onClose }) {
 									<input
 										type='name'
 										id='name'
-										className='shadow-sm bg-black/[.1] border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+										className='shadow-sm bg-black/[.1]  border-gray-300 text-gray-900 text-[16px] rounded-lg block w-full p-2.5 dark:bg-white/[.1] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
 										placeholder='Иван Иванов'
 										value={name}
 										onChange={(e) => setName(e.target.value)}
@@ -102,7 +102,7 @@ export default function CallBackForm({ open = false, onClose }) {
 									<input
 										type='phone'
 										id='phone'
-										className='shadow-sm bg-black/[.1] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+										className='shadow-sm bg-black/[.1]  text-gray-900 text-[16px] rounded-lg  block w-full p-2.5 dark:bg-white/[.1] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:shadow-sm-light'
 										placeholder='+7 777 77-77'
 										value={phone}
 										onChange={(e) => setPhone(e.target.value)}
@@ -118,7 +118,7 @@ export default function CallBackForm({ open = false, onClose }) {
 									<input
 										type='email'
 										id='email'
-										className='shadow-sm bg-black/[.1] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+										className='shadow-sm bg-black/[.1] border-gray-300 text-gray-900 text-[16px] rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-white/[.1] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
 										placeholder='example@mail.ru'
 										value={email}
 										onChange={(e) => setEmail(e.target.value)}
