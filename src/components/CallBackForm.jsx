64dee8d7cb855c93133d71f4a69aca39d48b58a5
AtikoWeb <dev.atikoweb@gmail.com>
2023-07-26 19:57:03 +0600

    dev

--- src/components/CallBackForm.jsx
+++ src/components/CallBackForm.jsx
@@ -101,7 +101,7 @@ export default function CallBackForm({ open = false, onClose, tarif = '' }) {
 							leaveFrom='opacity-100 translate-y-0 sm:scale-100'
 							leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
 						>
-							<Dialog.Panel className='relative transform w-screen p-12 overflow-hidden rounded-3xl backdrop-blur-2xl bg-white/[.6] dark:bg-[#1D222A]/[.8] text-left shadow-2xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
+							<Dialog.Panel className='relative transform w-screen p-12 overflow-hidden md:rounded-3xl rounded-t-3xl backdrop-blur-2xl bg-white/[.6] dark:bg-[#1D222A]/[.8] text-left shadow-2xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
 								<div
 									className='absolute right-2 top-2 btn btn-ghost btn-circle'
 									onClick={onClose}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,54 @@
 import React from 'react';
-import { AiFillYoutube, AiFillInstagram, AiFillFacebook } from 'react-icons/ai';
+import { FaPhoneAlt } from 'react-icons/fa';
+import { IoLogoWhatsapp } from 'react-icons/io';
+import { AiFillInstagram } from 'react-icons/ai';
 
 function Footer() {
 	return (
-		<footer className='footer footer-center p-10 bg-base-100'>
-			<div>
-				<span className='text-2xl font-bold '>Мы в соц сетях</span>
+		<section>
+			<div className='text-center text-4xl lg:text-5xl mt-20 mb-10 font-black'>
+				<span>Контакты</span>
 			</div>
-			<div>
-				<div className='grid grid-flow-col gap-4 -mt-5'>
-					<a>
-						<AiFillYoutube
-							size={40}
-							className='text-red-500'
-						/>
-					</a>
-					<a>
-						<AiFillInstagram
-							size={40}
-							className='text-rose-500'
-						/>
-					</a>
-					<a>
-						<a>
-							<AiFillFacebook
-								size={40}
-								className='text-blue-500'
-							/>
+			<section>
+				<div class='mx-auto max-w-screen-xl'>
+					<div class='flex justify-center items-center text-gray-500'>
+						<a
+							className='btn btn-lg w-24 h-20 rounded-xl md:w-36 md:h-32 mx-3 md:mx-5'
+							href='tel:+77086001010'
+						>
+							<FaPhoneAlt className='text-4xl md:text-5xl' />
 						</a>
-					</a>
+						<a
+							className='btn btn-lg w-24 h-20 md:w-36 md:h-32 bg-green-500 hover:bg-green-600 rounded-xl mx-3 md:mx-5'
+							href='https://wa.me/+77086001010?text=Здравствуйте! Хочу задать вопрос об автоматизации'
+						>
+							<IoLogoWhatsapp className='text-4xl md:text-6xl text-base-100' />
+						</a>
+						<a
+							className='btn btn-lg w-24 h-20 md:w-36 md:h-32 bg-gradient-to-bl from-fuchsia-500 via-red-600 to-orange-400 hover:from-fuchsia-600 hover:via-red-500 hover:to-orange-500 rounded-xl mx-3 md:mx-5'
+							href='https://www.instagram.com/atiko_web/'
+						>
+							<AiFillInstagram className='text-4xl md:text-6xl text-white' />
+						</a>
+					</div>
 				</div>
+			</section>
+
+			<iframe
+				src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2906.908716938562!2d76.92060601165777!3d43.232373479426506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x388369546ff87397%3A0x88f6f626eb8a19d7!2sAtikoWeb!5e0!3m2!1sru!2skz!4v1690374899794!5m2!1sru!2skz'
+				width='600'
+				height='450'
+				className='mx-auto w-full mt-14'
+				allowFullScreen=''
+				loading='lazy'
+				referrerPolicy='no-referrer-when-downgrade'
+			/>
+			<div className='p-10 text-center'>
+				<span class='text-2xl text-gray-400 dark:text-gray-500 font-bold'>
+					© 2023 AtikoWeb
+				</span>
 			</div>
-		</footer>
+		</section>
 	);
 }
 
